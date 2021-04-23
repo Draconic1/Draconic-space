ec3be2e1fb6a2501807f5843771bd913751eca4e
Angelina <71411417+Draconic1@users.noreply.github.com>
2021-04-23 18:55:07 +0300

    Add files via upload

--- DatabaseHelper.java
+++ DatabaseHelper.java
@@ -14,7 +14,7 @@ import java.io.OutputStream;
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static String DB_NAME = "coordnew.db";
     private static String DB_PATH = "";
-    private static final int DB_VERSION = 3;
+    private static final int DB_VERSION = 4;
 
     private SQLiteDatabase mDataBase;
     private final Context mContext;
